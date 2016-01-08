4272a7e2dbaa9066c6df189a4c5f6ee96cdcfd0a
ataibarkai <atai.barkai@gmail.com>
2016-01-08 04:59:25 -0800

    Initial commit

---
+++ CommonSource/Doublish.swift
@@ -0,0 +1,155 @@
+//
+//  Doublish.swift
+//  T-CIMB
+//
+//  Created by Atai Barkai on 1/6/16.
+//  Copyright © 2016 Atai Barkai. All rights reserved.
+//
+
+import Foundation
+
+
+protocol Doublish: Comparable, CustomStringConvertible, Hashable /*, FloatingPointType */ {
+	var value: Double {get set}
+	init()
+}
+
+
+// Providing the default constructor
+extension Doublish {
+	init(_ value: Double){
+		self.init()
+		self.value = value
+	}
+}
+
+// Comparable compliance
+func == <T where T: Doublish> (x: T, y: T) -> Bool {
+	return (x.value == y.value)
+}
+func < <T where T: Doublish> (x: T, y:T) -> Bool {
+	return x.value < y.value
+}
+
+
+// CustomStringConvertible compliance
+extension Doublish {
+	var description: String {
+		return self.value.description
+	}
+}
+
+
+// Hashable compliance
+extension Doublish {
+	var hashValue: Int {
+		return self.value.hashValue
+	}
+}
+
+
+// Addition
+func +<T where T: Doublish>(left: T, right: T) -> T {
+	return T.init((left.value + right.value))
+}
+
+// Subtract
+func -<T where T: Doublish>(left: T, right: T) -> T {
+	return T.init((left.value - right.value))
+}
+
+
+
+/*
+
+// Stridable compliance
+extension Doublish {
+	typealias Stride = Double
+	
+	func advancedBy(n: Double) -> Self{
+		return Self.init(self.value.advancedBy(n))
+	}
+	
+	func distanceTo(other: Self) -> Double{
+		return self.value.distanceTo(other.value)
+	}
+
+}
+
+// FloatingPointType compliance
+extension Doublish {
+	
+	// initializers
+	init(_ value: Int) {
+		self.init(Double(value))
+	}
+	init(_ value: Int8) {
+		self.init(Double(value))
+	}
+	init(_ value: Int16) {
+		self.init(Double(value))
+	}
+	init(_ value: Int32) {
+		self.init(Double(value))
+	}
+	init(_ value: Int64) {
+		self.init(Double(value))
+	}
+	init(_ value: UInt) {
+		self.init(Double(value))
+	}
+	init(_ value: UInt8) {
+		self.init(Double(value))
+	}
+	init(_ value: UInt16) {
+		self.init(Double(value))
+	}
+	init(_ value: UInt32) {
+		self.init(Double(value))
+	}
+	init(_ value: UInt64) {
+		self.init(Double(value))
+	}
+
+	// static variables
+	static var NaN: Self{
+		return self.init(Double.NaN)
+	}
+	static var infinity: Self{
+		return self.init(Double.infinity)
+	}
+	static var quietNaN: Self{
+		return self.init(Double.quietNaN)
+	}
+	
+	// instance variables
+	var floatingPointClass: FloatingPointClassification {
+		return self.value.floatingPointClass
+	}
+	var isFinite: Bool{
+		return self.value.isFinite
+	}
+	var isInfinite: Bool{
+		return self.value.isInfinite
+	}
+	var isNaN: Bool{
+		return self.value.isNaN
+	}
+	var isNormal: Bool{
+		return self.value.isNormal
+	}
+	var isSignMinus: Bool{
+		return self.value.isSignMinus
+	}
+	var isSignaling: Bool{
+		return self.value.isSignaling
+	}
+	var isSubnormal: Bool{
+		return self.value.isSubnormal
+	}
+	var isZero: Bool{
+		return self.value.isZero
+	}
+}
+
+*/
---
+++ CommonSource/Stringish.swift
@@ -0,0 +1,55 @@
+//
+//  Stringish.swift
+//  T-CIMB
+//
+//  Created by Atai Barkai on 1/6/16.
+//  Copyright © 2016 Atai Barkai. All rights reserved.
+//
+
+import Foundation
+
+protocol Stringish: Comparable, CustomStringConvertible, CustomDebugStringConvertible, Hashable{
+	var value: String {get set}
+	init()
+}
+
+// Providing the default constructor
+extension Stringish {
+	init(_ value: String){
+		self.init()
+		self.value = value
+	}
+}
+
+// Comparable compliance
+func == <T where T: Stringish> (x: T, y: T) -> Bool {
+	return
+		(x.dynamicType == y.dynamicType) && // To be equal, types must be invariant, not covariant
+		(x.value == y.value)
+}
+func < <T where T: Stringish> (x: T, y:T) -> Bool {
+	return x.value < y.value
+}
+
+// CustomStringConvertible compliance
+extension Stringish {
+	var description: String {
+		return self.value
+	}
+}
+
+// CustomDebugStringConvertible compliance
+extension Stringish {
+	var debugDescription: String {
+		return "(\(self.dynamicType)): \(self.value)"
+	}
+}
+
+// Hashable compliance
+extension Stringish {
+	var hashValue: Int {
+		return self.value.hash
+	}
+}
+
+
