251245b8037127339a843ab45b93df59bd9efab2
ataibarkai <atai.barkai@gmail.com>
2016-01-22 11:14:07 -0800

    Updated numbers-tests

--- TypeBurritoFrameworkTests/TypeBurrito-Numbers-Tests.swift
+++ TypeBurritoFrameworkTests/TypeBurrito-Numbers-Tests.swift
@@ -42,9 +42,7 @@ typealias IntWrapper = TypeBurrito<_IntWrapper>
 
 class TypeBurrito_Numbers_Tests: XCTestCase {
 	
-	
-	
-	func testNumberOvertypesCreation() {
+	func testCreation() {
 		
 		let myMass = Kg(87.4)
 		let myHeight = Meters(1.87)
@@ -53,25 +51,34 @@ class TypeBurrito_Numbers_Tests: XCTestCase {
 		XCTAssertEqual(myHeight.value, 1.87)
 	}
 	
-	func testNumberOvertypesAdditionSubtraction() {
+	func testAdditionSubtraction() {
 		
 		let num1_Double: Double = 3432.343
 		let num2_Double: Double = 234.5
 		let num1_DoubleWrapped = DoubleWrapper(num1_Double)
 		let num2_DoubleWrapped = DoubleWrapper(num2_Double)
-		XCTAssertEqual((num2_DoubleWrapped-num1_DoubleWrapped).value, num2_Double-num1_Double)
+		XCTAssertEqual(
+			(num2_DoubleWrapped-num1_DoubleWrapped).value,
+			num2_Double-num1_Double
+		)
 		
 		let num1_Float: Float = 3432.343
 		let num2_Float: Float = 234.5
 		let num1_FloatWrapped = FloatWrapper(num1_Float)
 		let num2_FloatWrapped = FloatWrapper(num2_Float)
-		XCTAssertEqual((num2_FloatWrapped-num1_FloatWrapped).value, num2_Float-num1_Float)
+		XCTAssertEqual(
+			(num2_FloatWrapped-num1_FloatWrapped).value,
+			num2_Float-num1_Float
+		)
 		
 		let num1_Int: Int = 42
 		let num2_Int: Int = 293
 		let num1_IntWrapped = IntWrapper(num1_Int)
 		let num2_IntWrapped = IntWrapper(num2_Int)
-		XCTAssertEqual((num2_IntWrapped-num1_IntWrapped).value, num2_Int-num1_Int)
+		XCTAssertEqual(
+			(num2_IntWrapped-num1_IntWrapped).value,
+			num2_Int-num1_Int
+		)
 		
 		// the following should produce compile-time errors:
 //		let anImpossibility = Kg(284) + Meters(34)
