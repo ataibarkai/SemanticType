f7036c27d78c373d81018b3df1cb1926e4c8acc0
ataibarkai <atai.barkai@gmail.com>
2016-01-22 10:04:52 -0800

    Refactored type requirements

--- CommonSource/TypeBurrito.swift
+++ CommonSource/TypeBurrito.swift
@@ -9,14 +9,10 @@
 import Foundation
 
 public protocol TypeBurritoSpec {
-	typealias TheTypeInsideTheBurrito
+	typealias TheTypeInsideTheBurrito: Comparable, CustomStringConvertible, Hashable
 }
 
-public struct TypeBurrito <Spec: TypeBurritoSpec where
-									Spec.TheTypeInsideTheBurrito: Comparable,
-									Spec.TheTypeInsideTheBurrito: CustomStringConvertible,
-									Spec.TheTypeInsideTheBurrito: Hashable>
-													: Equatable, Comparable, Hashable, CustomStringConvertible, CustomDebugStringConvertible {
+public struct TypeBurrito <Spec: TypeBurritoSpec>: Comparable, Hashable, CustomStringConvertible, CustomDebugStringConvertible {
 	
 	public var value: Spec.TheTypeInsideTheBurrito
 	
@@ -51,57 +47,5 @@ public func < <Spec: TypeBurritoSpec>
 	(left: TypeBurrito<Spec>, right: TypeBurrito<Spec>) -> Bool {
 		return left.value < right.value
 }
-
-
-/*
-public protocol TypeBurrito: Comparable, CustomStringConvertible, CustomDebugStringConvertible, Hashable {
-	
-	typealias UnderlyingValueType: Comparable, CustomStringConvertible, Hashable
-	
-	/// The value wrapped by this TypeBurrito subtype
-	var value: UnderlyingValueType {get set}
-	init()
-}
-
-
-// Providing the default constructor
-public extension TypeBurrito {
-	init(_ value: UnderlyingValueType){
-		self.init()
-		self.value = value
-	}
-}
-
-// Comparable compliance
-public func == <T where T: TypeBurrito> (x: T, y: T) -> Bool {
-	return
-			(x.dynamicType == y.dynamicType) && // To be equal, types must be invariant, not covariant
-			(x.value == y.value)
-}
-
-public func < <T where T: TypeBurrito> (x: T, y:T) -> Bool {
-	return x.value < y.value
-}
-
-// CustomStringConvertible compliance
-public extension TypeBurrito {
-	var description: String {
-		return self.value.description
-	}
-}
-
-// CustomDebugStringConvertible compliance
-public extension TypeBurrito {
-	var debugDescription: String {
-		return "(\(self.dynamicType)): \(self.value)"
-	}
-}
-
-// Hashable compliance
-public extension TypeBurrito {
-}
-
-
-*/
 
 
