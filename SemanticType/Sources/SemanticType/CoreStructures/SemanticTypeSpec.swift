15867da2c572e0546965412c4de5cb7b781be31d
Atai Barkai <atai.barkai@gmail.com>
2019-08-25 19:13:07 -0700

    create `GeneralizedSemanticTypeSpec` and task-specific subprotocols

--- SemanticType/Sources/SemanticType/CoreStructures/SemanticTypeSpec.swift
+++ SemanticType/Sources/SemanticType/CoreStructures/SemanticTypeSpec.swift
@@ -5,7 +5,7 @@
 //  Created by Atai Barkai on 8/2/19.
 //
 
-public protocol SemanticTypeSpec {
+public protocol GeneralizedSemanticTypeSpec {
     
     /// The type of the primitive value wrapped by the `SemanticType`.
     ///
@@ -13,14 +13,43 @@ public protocol SemanticTypeSpec {
     /// not modified by outside forces after passing through the `SemanticType`'s `gatewayMap` function
     /// and becoming stored inside of a `SemanticType` instance.
     associatedtype BackingPrimitiveWithValueSemantics
+    associatedtype GatewayMetadataWithValueSemantics
     associatedtype Error: Swift.Error
     
     /// - Tag: SemanticTypeSpec.gatewayMap
     static func gatewayMap(
        preMap: BackingPrimitiveWithValueSemantics
-    ) -> Result<BackingPrimitiveWithValueSemantics, Error>
+    ) -> Result<(BackingPrimitiveWithValueSemantics, GatewayMetadataWithValueSemantics), Error>
 }
 
+
+/// A `SemanticTypeSpec` with no gateway metadata.
+public protocol SemanticTypeSpec: GeneralizedSemanticTypeSpec where GatewayMetadataWithValueSemantics == () {
+    static func gatewayMap(
+       preMap: BackingPrimitiveWithValueSemantics
+    ) -> Result<BackingPrimitiveWithValueSemantics, Error>
+}
+extension SemanticTypeSpec {
+    public static func gatewayMap(
+       preMap: BackingPrimitiveWithValueSemantics
+    ) -> Result<(BackingPrimitiveWithValueSemantics, GatewayMetadataWithValueSemantics), Error> {
+        return gatewayMap(preMap: preMap).map { ($0, ()) }
+    }
+}
+
+/// A `SemanticTypeSpec` whose `gatewayMap` function never errors.
+public protocol ErrorlessSemanticTypeSpec: SemanticTypeSpec where Error == Never {
+    static func gatewayMap(
+       preMap: BackingPrimitiveWithValueSemantics
+    ) -> BackingPrimitiveWithValueSemantics
+}
+extension ErrorlessSemanticTypeSpec {
+    public static func gatewayMap(
+       preMap: BackingPrimitiveWithValueSemantics
+    ) -> Result<BackingPrimitiveWithValueSemantics, Error> {
+        return .success(gatewayMap(preMap: preMap))
+    }
+}
 
 // This extension provides the **identiy** map as the default gatewayMap implementation
 // for error-less semantic types.
