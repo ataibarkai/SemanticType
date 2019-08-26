ac21fefa02db3ecfa5d5bfefef97aa1bd0984f68
Atai Barkai <atai.barkai@gmail.com>
2019-08-25 19:12:48 -0700

    comment out placeholder code

--- SemanticType/Tests/SemanticTypeTests/SemanticType+ConditionalProtocolConformances/UniversallyApplicableConformancesTests.swift
+++ SemanticType/Tests/SemanticTypeTests/SemanticType+ConditionalProtocolConformances/UniversallyApplicableConformancesTests.swift
@@ -32,26 +32,26 @@ final class SemanticType_ConditioinalProtocolConformances_UniversallyApplicableC
         XCTAssertEqual("\(uniquelyStrangeBobString)", "Bob... how strange...")
     }
     
-    func testCustomStringConvertibleConformance() {
-        struct StrangeString: CustomStringConvertible {
-            var str: String
-            
-            var description: String {
-                "\(str)... how strange..."
-            }
-        }
-        enum UniquelyStrangeString_Spec: SemanticTypeSpec {
-            typealias BackingPrimitiveWithValueSemantics = StrangeString
-            typealias Error = Never
-            static func gatewayMap(preMap: StrangeString) -> Result<StrangeString, Never> {
-                return .success(preMap)
-            }
-        }
-        typealias UniquelyStrangeString = SemanticType<UniquelyStrangeString_Spec>
-        
-        let uniquelyStrangeBobString = UniquelyStrangeString(.init(str: "Bob"))
-        XCTAssertEqual("\(uniquelyStrangeBobString)", "Bob... how strange...")
-    }
+//    func testCustomStringConvertibleConformance() {
+//        struct StrangeString: CustomStringConvertible {
+//            var str: String
+//            
+//            var description: String {
+//                "\(str)... how strange..."
+//            }
+//        }
+//        enum UniquelyStrangeString_Spec: SemanticTypeSpec {
+//            typealias BackingPrimitiveWithValueSemantics = StrangeString
+//            typealias Error = Never
+//            static func gatewayMap(preMap: StrangeString) -> Result<StrangeString, Never> {
+//                return .success(preMap)
+//            }
+//        }
+//        typealias UniquelyStrangeString = SemanticType<UniquelyStrangeString_Spec>
+//        
+//        let uniquelyStrangeBobString = UniquelyStrangeString(.init(str: "Bob"))
+//        XCTAssertEqual("\(uniquelyStrangeBobString)", "Bob... how strange...")
+//    }
     
     static var allTests = [
         ("testCustomStringConvertibleConformance", testCustomStringConvertibleConformance),
