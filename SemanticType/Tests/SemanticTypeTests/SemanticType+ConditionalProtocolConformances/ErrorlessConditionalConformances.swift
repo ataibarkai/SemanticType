2a04692138f9acabddb9af54d01a02bea2a8f801
Atai Barkai <atai.barkai@gmail.com>
2019-09-08 07:15:22 -0700

    testAdditiveArithmeticConformance

--- SemanticType/Tests/SemanticTypeTests/SemanticType+ConditionalProtocolConformances/ErrorlessConditionalConformances.swift
+++ SemanticType/Tests/SemanticTypeTests/SemanticType+ConditionalProtocolConformances/ErrorlessConditionalConformances.swift
@@ -13,12 +13,37 @@ final class SemanticType_ConditioinalProtocolConformances_ErrorlessConditionalCo
     
     func testAdditiveArithmeticConformance() {
         enum Seconds_Spec: ErrorlessSemanticTypeSpec {
-
+            typealias BackingPrimitiveWithValueSemantics = Double
             static func gateway(preMap: Double) -> Double {
                 return preMap
             }
-            
         }
+        typealias Seconds = SemanticType<Seconds_Spec>
+        
+        var step1Duration: Seconds = 5
+        let step2Duration: Seconds = 10
+        
+        XCTAssertEqual(
+            step1Duration + step2Duration,
+            Seconds(15)
+        )
+        
+        step1Duration += 7
+        XCTAssertEqual(
+            step1Duration,
+            Seconds(12)
+        )
+        
+        XCTAssertEqual(
+            step1Duration - step2Duration,
+            Seconds(2)
+        )
+        
+        step1Duration -= 9
+        XCTAssertEqual(
+            step1Duration,
+            Seconds(3)
+        )
     }
     
     static var allTests = [
