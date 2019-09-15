e58246d7054ddcb081439f7ed3e83dd014fe492f
Atai Barkai <atai.barkai@gmail.com>
2019-09-15 12:57:39 -0700

    testNumericConformance

--- SemanticType/Tests/SemanticType_Tests/SemanticType_ConditionalExtensions_Tests/DoublyConditionalConformances/ErrorlessSemanticType_Numeric_Tests.swift
+++ SemanticType/Tests/SemanticType_Tests/SemanticType_ConditionalExtensions_Tests/DoublyConditionalConformances/ErrorlessSemanticType_Numeric_Tests.swift
@@ -3,12 +3,28 @@ import XCTest
 
 final class ErrorlessSemanticType_Numeric_Tests: XCTestCase {
     
-    func testSample() {
+    func testNumericConformance() {
+        enum UserEnteredInt_Spec: ErrorlessSemanticTypeSpec, ShouldBeNumeric {
+            typealias BackingPrimitiveWithValueSemantics = Int
+            static func gateway(preMap: Int) -> Int {
+                return preMap
+            }
+        }
+        typealias UserEnteredInt = SemanticType<UserEnteredInt_Spec>
         
+        let five = UserEnteredInt(5)
+        let six = UserEnteredInt(6)
+        
+        let thirty = five * six
+        XCTAssertEqual(thirty, UserEnteredInt(30))
+        
+        var finalNumber = UserEnteredInt(10)
+        finalNumber *= six
+        XCTAssertEqual(finalNumber, UserEnteredInt(60))
     }
     
     static var allTests = [
-        ("testSample", testSample),
+        ("testNumericConformance", testNumericConformance),
     ]
 
 }
