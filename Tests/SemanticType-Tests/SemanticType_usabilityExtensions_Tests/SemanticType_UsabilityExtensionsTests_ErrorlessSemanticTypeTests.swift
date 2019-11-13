95ef5a3baadac317b71bc2cd6cbfdd170eca24bb
Atai Barkai <atai.barkai@gmail.com>
2019-11-12 23:17:05 -0800

    simplify errorless tests

--- Tests/SemanticType-Tests/SemanticType_usabilityExtensions_Tests/SemanticType_UsabilityExtensionsTests_ErrorlessSemanticTypeTests.swift
+++ Tests/SemanticType-Tests/SemanticType_usabilityExtensions_Tests/SemanticType_UsabilityExtensionsTests_ErrorlessSemanticTypeTests.swift
@@ -3,6 +3,10 @@ import XCTest
 
 final class SemanticType_UsabilityExtensionsTests_ErrorlessSemanticTypeTests: XCTestCase {
     
+    // Dollars:
+    enum Dollars_Spec: ErrorlessSemanticTypeSpec { typealias RawValue = Int }
+    typealias Dollars = SemanticType<Dollars_Spec>
+
     // CaselessString:
     enum CaselessString_Spec: ErrorlessSemanticTypeSpec {
         typealias RawValue = String
@@ -12,32 +16,20 @@ final class SemanticType_UsabilityExtensionsTests_ErrorlessSemanticTypeTests: XC
         }
     }
     typealias CaselessString = SemanticType<CaselessString_Spec>
-    
-    // Dollars:
-    enum Dollars_Spec: ErrorlessSemanticTypeSpec {
-        typealias RawValue = Int
-        typealias Error = Never
-        static func gateway(preMap: Int) -> Int {
-            return preMap
-        }
-    }
-    typealias Dollars = SemanticType<Dollars_Spec>
-    
-    
+        
     // ProcessedContactFormInput_Spec:
     struct ContactFormInput {
         var email: String
         var message: String
     }
-    enum ProcessedContactFormInput_Spec: ValidatedSemanticTypeSpec {
+    enum ProcessedContactFormInput_Spec: ErrorlessSemanticTypeSpec {
         typealias RawValue = ContactFormInput
-        typealias Error = Never
         
-        static func gateway(preMap: ContactFormInput) -> Result<ContactFormInput, Never> {
-            return .success(.init(
+        static func gateway(preMap: ContactFormInput) -> ContactFormInput {
+            return .init(
                 email: preMap.email.lowercased(),
                 message: preMap.message
-            ))
+            )
         }
     }
     typealias ProcessedContactFormInput = SemanticType<ProcessedContactFormInput_Spec>
@@ -47,8 +39,6 @@ final class SemanticType_UsabilityExtensionsTests_ErrorlessSemanticTypeTests: XC
     func testInitialization() {
         let hello = CaselessString("HELlo")
         XCTAssertEqual(hello._rawValue, "hello")
-        
-        let b: CaselessString = "HeLlo"
     }
     
     
